4bc285c9d6b46556521c6cb54587523f7d23e7b6
PhantomHacker219 <jbrodi766@gmail.com>
2025-03-05 17:56:12 +0500

    media screen

--- app.js
+++ app.js
@@ -27,21 +27,12 @@
 
 
  document.addEventListener("DOMContentLoaded", () => {
-    let scrollY = 0;
-    const initialHeight = window.visualViewport ? window.visualViewport.height : window.innerHeight;
-    window.visualViewport.addEventListener("resize", () => {
-      const currentHeight = window.visualViewport ? window.visualViewport.height : window.innerHeight;
-      if (initialHeight - currentHeight > 150) {
-        scrollY = window.scrollY;
-        document.body.classList.add("keyboard-fixed");
-        document.body.style.position = "fixed";
-        document.body.style.top = `-${scrollY}px`;
-      } else {
-        document.body.classList.remove("keyboard-fixed");
-        document.body.style.position = "";
-        document.body.style.top = "";
-        window.scrollTo(0, scrollY);
-      }
+    const inputs = document.querySelectorAll("#registration-form input");
+    inputs.forEach(input => {
+      input.addEventListener("pointerdown", e => {
+        e.preventDefault();
+        input.focus({ preventScroll: true });
+      });
     });
   });
   
